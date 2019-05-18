344216bbf7cba6ec5f8d65c8cabc9051c59d1353
Irina Sedova <irihka.sedova2011@yandex.ru>
2019-05-18 13:20:30 +0500

    first structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './redux/reducers'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Provider store={createStore(reducer)}>
+        <BrowserRouter>
+            <Switch>
+                {/*<Route path="/create/event" component={CreateEvent}/>*/}
+                {/*<Route path="/card" component={Card}/>*/}
+                {/*<Route path="/cards" component={CardsList}/>*/}
+                {/*<Route exact path="/" component={Main}/>*/}
+                {/*<Route component={NotFound}/>*/}
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
+
 serviceWorker.unregister();
