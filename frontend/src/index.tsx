4e9be51c434bb558261c905155ce5f5e19f53015
ubogov <s.ubogov@skbkontur.ru>
2019-05-18 14:15:11 +0500

    вернул редакс

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import CreateEvent from './pages/CreateEvent'
@@ -11,18 +13,22 @@ import Main from './pages/Main'
 import NotFound from './pages/NotFound'
 import Layout from "./common-components/Layout";
 
+import reducer from './redux/reducers'
+
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Layout>
-            <Route path="/create/event" component={CreateEvent}/>
-            <Route path="/card" component={Card}/>
-            <Route path="/cards" component={CardsList}/>
-            <Route exact path="/" component={Main}/>
-            <Route component={NotFound}/>
-            </Layout>
-        </Switch>
-    </BrowserRouter>
+    <Provider store={createStore(reducer)}>
+        <BrowserRouter>
+            <Switch>
+                <Layout>
+                <Route path="/create/event" component={CreateEvent}/>
+                <Route path="/card" component={Card}/>
+                <Route path="/cards" component={CardsList}/>
+                <Route exact path="/" component={Main}/>
+                <Route component={NotFound}/>
+                </Layout>
+            </Switch>
+        </BrowserRouter>
+    </Provider>
     , document.getElementById('root'));
 
 serviceWorker.unregister();
