fe7f0c27ca81ca799a0eff70257488b58b4f9a48
ubogov <s.ubogov@skbkontur.ru>
2019-05-18 20:53:38 +0500

    добавил переход на страницу с фильтрами и на главную

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -2,14 +2,23 @@ import React, {Component} from 'react';
 import styles from './header.module.css';
 import FilterList from '@material-ui/icons/FilterList';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import {Link} from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
             <header className={styles.header}>
-                <div className={styles.text}>Куда<br />пойдем?</div>
+                <div className={styles.text}>
+                    <Link to={'/'}>
+                        Куда<br />пойдем?
+                    </Link>
+                </div>
                 <div className={styles.rightBlock}>
-                    <div onClick={this.onFilterClick} className={styles.filterIcon}><FilterList fontSize="inherit" /></div>
+                    <Link to={'/filters'}>
+                        <div onClick={this.onFilterClick} className={styles.filterIcon}>
+                            <FilterList fontSize="inherit" />
+                        </div>
+                    </Link>
                     <div onClick={this.onProfileClick} className={styles.profileIcon}><AccountCircle fontSize="inherit" /></div>
                 </div>
             </header>
