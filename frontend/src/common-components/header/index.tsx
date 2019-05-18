26e1f5d7b5896abb22221033205a037a16c16e9b
ubogov <s.ubogov@skbkontur.ru>
2019-05-18 16:24:16 +0500

    сделал header

--- frontend/src/common-components/header/index.tsx
+++ frontend/src/common-components/header/index.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './Header.module.css';
+import {FilterList} from '@material-ui/icons';
+import {AccountCircle} from '@material-ui/icons';
 
-export default function Header() {
-    return <header className={styles.header}>header</header>;
+export default class Header extends Component {
+    render() {
+        return <header className={styles.header}>
+            <div className={styles.text}>Куда<br />пойдем?</div>
+            <div className={styles.rightBlock}>
+                <div onClick={this.onFilterClick} className={styles.filterIcon}><FilterList fontSize={"inherit"}/></div>
+                <div onClick={this.onProfileClick} className={styles.profileIcon}><AccountCircle fontSize={"inherit"}/></div>
+            </div>
+        </header>;
+    }
+
+    onFilterClick = () => {
+        console.info('click on filter');
+    };
+
+    onProfileClick = () => {
+        console.info('click on profile');
+    }
 }
