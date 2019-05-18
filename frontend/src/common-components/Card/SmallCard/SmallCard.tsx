e3d9a7d4cc2078561e5b90dc1661793eacc2db8a
Седова Ирина <irinasedova2011@gmail.com>
2019-05-18 16:50:44 +0500

    Card (#14)
    card

---
+++ frontend/src/common-components/Card/SmallCard/SmallCard.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import styles from './SmallCard.module.css'
+
+interface Filter {
+    id: string
+    name: string
+}
+
+interface EventProp {
+    title: string
+    date: string
+    image: string
+    description: string
+    filters: Filter[]
+    countLikes: number
+}
+
+
+
+const SmallCard: React.FC<EventProp> = ({ title, date, image, description, filters, countLikes }) => {
+
+    return (
+        <Card className={styles.smallCard_card}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="Avatar">
+                        R
+                    </Avatar>
+                }
+                title={title}
+                subheader={date}
+            />
+            <CardMedia
+                className={styles.smallCard_media}
+                image={image}
+            />
+            <CardContent>
+                <Typography component="p">
+                    {description}
+                </Typography>
+            </CardContent>
+            <CardContent>
+                <div className={styles.smallCard_actionBlock}>
+                    <div className={styles.smallCard_filters}>
+                        {filters.map(filter => (
+                            <Link component="a" href={`/events?${filter.id}`}>
+                                <Typography component="p" className={styles.smallCard_filter}>
+                                    {filter.name}
+                                </Typography>
+                            </Link>
+                        ))}
+                    </div>
+                    <div className={styles.smallCard_likes}>
+                        <Typography component="p" className={styles.smallCard_count_likes}>
+                            {countLikes}
+                        </Typography>
+                        <IconButton aria-label="Add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default SmallCard;
--- frontend/src/pages/CardsList/CardsList.tsx
+++ frontend/src/pages/CardsList/CardsList.tsx
@@ -1,5 +1,36 @@
-import React from 'react'
+import React from 'react';
+import SmallCard from '../../common-components/Card/SmallCard';
 
-const CardsList = () => (<div>CardList</div>);
+const testData = {
+    title: 'Shri mp and Chorizo Paella',
+    date: 'September 14, 2016',
+    image: '',
+    description: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
+    filters: [{
+        id: '1',
+        name: 'Sport',
+    }, {
+        id: '2',
+        name: 'Free',
+    }],
+    countLikes: 54,
+}
+
+const events = [testData, testData, testData]
+
+const CardsList = () => (
+    <div>
+        {events.map(event => (
+            <SmallCard
+                title={event.title}
+                date={event.date}
+                image={event.image}
+                description={event.description}
+                filters={event.filters}
+                countLikes={event.countLikes}
+            />
+        ))}
+    </div>
+);
 
 export default CardsList
