76afe966d74196966cf36f91c10becd74c3c37c7
ubogov <s.ubogov@skbkontur.ru>
2019-05-18 13:42:21 +0500

    удалил лишнее

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './App.css';
-import Header from "./common-components/Header";
-
-export default function App() {
-  return <Header />;
-};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import reducer from './redux/reducers'
+import Header from "./common-components/Header";
 
 ReactDOM.render(
-    <Provider store={createStore(reducer)}>
-        <BrowserRouter>
-            <Switch>
-                {/*<Route path="/create/event" component={CreateEvent}/>*/}
-                {/*<Route path="/card" component={Card}/>*/}
-                {/*<Route path="/cards" component={CardsList}/>*/}
-                {/*<Route exact path="/" component={Main}/>*/}
-                {/*<Route component={NotFound}/>*/}
-            </Switch>
-        </BrowserRouter>
-    </Provider>
+    <Header />
     , document.getElementById('root'));
 
 serviceWorker.unregister();
