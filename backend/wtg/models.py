07d402088179891b2a30dbbb219f58c00bfad1ce
Дима Старков <dvstark@yandex.ru>
2019-05-18 17:11:15 +0500

    Get events view

--- backend/wtg/models.py
+++ backend/wtg/models.py
@@ -5,3 +5,27 @@ class Tag(models.Model):
     slug = models.fields.SlugField()
     title = models.TextField(db_index=True)
     description = models.TextField(db_index=True)
+
+    def __str__(self):
+        return f'Tag {self.slug}'
+
+
+class Event(models.Model):
+    slug = models.fields.SlugField()
+    title = models.TextField(db_index=True)
+    description = models.TextField(db_index=True, default='')
+
+    tags = models.ManyToManyField(Tag, blank=True)
+    similar_with = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+    image = models.ImageField()
+
+    likes_count = models.PositiveIntegerField(default=0, editable=False)
+    participants_count = models.PositiveIntegerField(default=0, editable=False)
+    max_participants_count = models.PositiveIntegerField(null=True)
+
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Event {self.slug}'
--- backend/wtg/serializers.py
+++ backend/wtg/serializers.py
@@ -6,9 +6,43 @@ from . import models
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
+        fields = '__all__'
+
+
+class EventTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Tag
         fields = (
-            'id',
+            'slug',
+            'title',
+        )
+
+
+class SimilarEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Event
+        fields = (
+            'slug',
+            'title',
+            'image',
+        )
+
+
+class EventSerializer(serializers.ModelSerializer):
+    tags = EventTagSerializer(many=True)
+    similar_with = SimilarEventSerializer(many=True)
+
+    class Meta:
+        model = models.Event
+        fields = (
             'slug',
             'title',
             'description',
+            'tags',
+            'similar_with',
+            'image',
+            'likes_count',
+            'participants_count',
+            'max_participants_count',
+            'date',
         )
