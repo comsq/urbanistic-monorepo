b3623c2fea89b9e5b8b8953ebb56b04a65078330
Дима Старков <dvstark@yandex.ru>
2019-05-18 15:08:26 +0500

    Static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +131,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
