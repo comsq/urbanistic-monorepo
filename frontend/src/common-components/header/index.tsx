687cca6ac8c393274116409f9a07a3653a01a0d7
ubogov <s.ubogov@skbkontur.ru>
2019-05-18 15:14:19 +0500

    добавил стили к хеадеру

--- frontend/src/common-components/header/index.tsx
+++ frontend/src/common-components/header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './Header.module.css';
 
 export default function Header() {
-    return <div>header</div>;
+    return <header className={styles.header}>header</header>;
 }
