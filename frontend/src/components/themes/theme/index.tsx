b247b552fbb91d2a55eaa14b8bebcc1d33171e3d
Sergey Ubogov <sergeiubogov@mail.ru>
2019-05-19 06:07:18 +0500

    сделал чтоб фильтры сохранялись при повторном переходе на страницу фильтров (#70)

--- frontend/src/components/themes/theme/index.tsx
+++ frontend/src/components/themes/theme/index.tsx
@@ -3,25 +3,34 @@ import Checkbox from "@material-ui/core/Checkbox";
 import CheckboxIcon from '../checkboxIcon';
 import {connect} from 'react-redux';
 import {selectTags} from "../../../redux/tags/actions";
+import {SubStore} from "../../../pages/main";
 
 type Props = {
     slug: string,
     title: string,
-    selectTags: any
+    selectTags: any,
+    selectedSlugs: Set<string>
 }
 type State = {
     checked: boolean
 }
 
 class Theme extends Component<Props, State> {
-    state = {
-        checked: false
-    };
+    constructor(props: Props) {
+        super(props);
+
+        const {selectedSlugs} = props;
+        const checked = Array.from(selectedSlugs).includes(props.slug);
+        this.state = {
+            checked
+        }
+    }
+
     render() {
         const {title, slug} = this.props;
         const {checked} = this.state;
         const icon = <CheckboxIcon checked={checked} slug={slug} title={title} />;
-        return <Checkbox onChange={this.onChange} checkedIcon={icon} icon={icon} />;
+        return <Checkbox checked={checked} onChange={this.onChange} checkedIcon={icon} icon={icon} />;
     }
 
     onChange = (event: any, checked: boolean) => {
@@ -31,7 +40,12 @@ class Theme extends Component<Props, State> {
     }
 }
 
+function mapStateToProps(state: SubStore) {
+    const {selectedSlugs} = state.tags;
+
+    return {selectedSlugs};
+}
 function mapDispatchToProps(dispatch: any) {
     return {selectTags: ({slug, checked}: {slug: string, checked: boolean}) => dispatch(selectTags({slug, checked}))}
 }
-export default connect(null, mapDispatchToProps)(Theme);
+export default connect(mapStateToProps, mapDispatchToProps)(Theme);
